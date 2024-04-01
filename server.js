17da275d9b7a3acbb0775be7b1c0b59383033990
Your Name <you@example.com>
2024-04-01 05:48:39 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid');//Import uuid module
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//Serve static files to public directory
 app.use(express.static('public'));
 
 // Define routes
@@ -22,7 +24,7 @@ app.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return next (err);//Pass error handler to error middleware
     }
     res.json(JSON.parse(data));
   });
@@ -34,16 +36,25 @@ app.post('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return next(err);
     }
     const notes = JSON.parse(data);
     notes.push(newNote);
     fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return next(err);//Pass the err to error handler middleware
       }
-      res.status(201).json(newNote);
+
+
+//  res.cookie('cookieName', 'cookieValue', {
+  // sameSite: 'none',
+  // secure: true,
+  // domain: '.example.com', // Replace with your domain
+  // path: '/' // Replace with your desired path
+// });
+// 
+      // res.status(201).json(newNote);
     });
   });
 });
@@ -54,7 +65,7 @@ app.delete('/api/notes/:id', (req, res) => {
   fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return next(err);//Pass error to error handler middleware
     }
 
     let notes = JSON.parse(data);
@@ -63,14 +74,20 @@ app.delete('/api/notes/:id', (req, res) => {
     fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return next (err);//Pass error to error handler middleware
       }
       res.status(200).json({ message: 'Note deleted successfully' });
     });
   });
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+})
